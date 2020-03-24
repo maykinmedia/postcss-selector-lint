859347e2b921ba9bc4f26fe7e785916f6d80850d
Sven van de Scheur <svenvandescheur@gmail.com>
2020-03-24 11:15:52 +0100

    :white_check_mark: - Add test for issue #1.

---
+++ test/issues.js
@@ -0,0 +1,29 @@
+const chai = require('chai');
+const postcss = require('postcss');
+const selectorLint = require('../src/index');
+
+const {assert} = chai;
+
+describe('postcss-selector-lint (issues)', () => {
+    it('issue #1: Usage of keyframes result in invalid CSS message.', () => {
+        const input = `
+            .foo {
+              @keyframes spin {
+                0% {
+                  transform: rotate(0deg);
+                }
+                100% {
+                  transform: rotate(360deg);
+                }
+              }
+            }
+        `;
+
+        return postcss([selectorLint()]).process(input, {from: undefined})
+            .then(result => {
+                const warnings = result.warnings();
+                assert.lengthOf(warnings, 0);
+            })
+            .catch(e => assert.notOk(e.message));
+    });
+});
